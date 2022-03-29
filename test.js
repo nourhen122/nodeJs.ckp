4df835cba90c100ed08645717dee92db7423318e
nourhen122 <nourhabh123@gmail.com>
2022-03-29 15:42:38 +0100

    node.js

---
+++ test.js
@@ -0,0 +1,14 @@
+const http = require ('http')
+const port = 3000
+
+const server = http.createServer (function (req,res ){
+res.write('hello node')
+res.end('')
+})
+server.listen (port,function(error){
+    if(error){
+        console.log ('error')
+    } else {
+        console.log('server is listening on port'+port)
+    }
+})
